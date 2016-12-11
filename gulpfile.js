4d47d000dff1bded07dfbc0807d4a96638c25ca1
dieffrei <dieffrei.quadros@topinformation.com.br>
2016-12-11 17:51:00 -0200

    structure refactor

--- gulpfile.js
+++ gulpfile.js
@@ -14,30 +14,11 @@ gulp.task('clear', function() {
 
 gulp.task('copy', function() {
 
-    //gulp.src('bower_components/salesforce-lightning-design-system/assets/**').pipe(gulp.dest('dist/assets'));
     gulp.src(['!bower_components/salesforce-lightning-design-system/**', 'bower_components/**/**.*'])
         .pipe(gulp.dest('dist/bower_components'));
-
     gulp.src('bower_components/salesforce-lightning-design-system/assets/**/**.*').pipe(gulp.dest('dist/assets'));
-
-    gulp.src('*.html').pipe(gulp.dest('dist'));
-
-    gulp.src('js/**/*.js').pipe(gulp.dest('dist/js'));
-
-    gulp.src('icon/**.*').pipe(gulp.dest('dist/icon'));
-
+    gulp.src('src/**/**.*').pipe(gulp.dest('dist/'));
     gulp.src('*.json').pipe(gulp.dest('dist/'));
-
-    /*gulp.src('dist/js/*.js')
-        .pipe(minify({
-            ext:{
-                src:'-debug.js',
-                min:'.js'
-            },
-            exclude: ['bower_components']
-            //ignoreFiles: ['.combo.js', '-min.js']
-        }))
-        .pipe(gulp.dest('dist/min'))*/
 
 });
 
